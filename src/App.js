69eedf6582fb1db89255f7c79a4487f4e0240151
DexSus <insomniadex@gmail.com>
2024-12-04 11:12:52 +0200

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import "./index.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { LoginPage } from "./pages/loginPage/index.js";
+import { RegistrationPage } from "./pages/regPage/index.jsx";
+import { HomePage } from "./pages/homePage/index.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const isAuthenticated = () => {
+  return localStorage.getItem("authToken") !== null;
+};
+
+const LogOut =() => {
+  localStorage.removeItem("authToken");
+  localStorage.removeItem("authUser");
+  return <Navigate to="/login" />
 }
 
-export default App;
+const ProtectedRoute = ({ children }) => {
+  const isAuth = isAuthenticated();
+  const isAuthPage = ["LoginPage", "RegistrationPage"].includes(children.type.name);
+
+  if (isAuth && isAuthPage) {
+    return <Navigate to="/" />;
+  }
+
+  return isAuth ? children : <Navigate to="/login" />;
+};
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/registration" element={<RegistrationPage />} />
+
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <HomePage/>
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="*" element={<Navigate to="/login" />} />
+        <Route path="/logout" element={<LogOut/>} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
