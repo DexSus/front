10d3747aaffd1107908d490776d429ba3cf72a3c
DexSus <insomniadex@gmail.com>
2024-12-05 16:44:26 +0200

    (Fix) Bug fix UsedResources

--- src/pages/homePage/components/useResources/index.jsx
+++ src/pages/homePage/components/useResources/index.jsx
@@ -14,7 +14,7 @@ export const UseResources = () => {
     const user = JSON.parse(localStorage.getItem("authUser"));
     setUserUnitId(user?.unitId);
 
-    fetch("http://localhost:4000/api/resources")
+    fetch(`${process.env.REACT_APP_API_URL}/api/resources`)
       .then((response) => response.json())
       .then((data) => {
         const filteredResources = data.filter(
